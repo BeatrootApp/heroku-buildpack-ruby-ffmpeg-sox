9f36e3d7b808626b81afd6bc9720e5e064ccfc2c
Mark Schneider <mark@wevue.com>
2015-03-06 03:54:12 -0500

    Add ffmpeg, sox, aws, vim, exiftool.

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -286,6 +286,8 @@ SHELL
       set_env_default  "LANG",     "en_US.UTF-8"
       set_env_override "GEM_PATH", "$HOME/#{slug_vendor_base}:$GEM_PATH"
       set_env_override "PATH",     binstubs_relative_paths.map {|path| "$HOME/#{path}" }.join(":") + ":$PATH"
+      set_env_override "PATH",     "$HOME/vendor/bin:$PATH"
+      set_env_override "LD_LIBRARY_PATH", "$HOME/vendor/ffmpeg/lib:$HOME/vendor/sox/lib:$LD_LIBRARY_PATH"
 
       add_to_profiled set_default_web_concurrency if env("SENSIBLE_DEFAULTS")
 
